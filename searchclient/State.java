29cd238226463666575569b5d070c7aca8eb562f
osta0005 <olinestaerke@hotmail.com>
2021-02-07 13:27:58 +0100

    Fixed bugs

--- searchclient/State.java
+++ searchclient/State.java
@@ -246,7 +246,7 @@ public class State
                 box = this.boxes[destinationRow][destinationCol];
                 if (boxRow<boxes.length && boxRow>=0 && boxCol>=0 && boxCol<boxes[0].length && box!=0) {
                     Boolean color = (agentColor.toString() == boxColors[agent].toString());
-                    return this.cellIsFree(boxRow, boxCol) && color; //&& color; //&& agentColor.toString()==boxColors[box].toString();
+                    return this.cellIsFree(boxRow, boxCol); //&& color; //&& agentColor.toString()==boxColors[box].toString();
                 }
                 else return false;
 
@@ -258,8 +258,8 @@ public class State
                 boxCol = destinationCol ;
                 box = this.boxes[destinationRow-action.boxRowDelta][destinationCol-action.boxColDelta];
                 if (boxRow<boxes.length && boxRow>=0 && boxCol>=0 && boxCol<boxes[0].length && box!=0) {
-                    Boolean color = (agentColor.toString() == boxColors[agent].toString());
-                    return this.cellIsFree(boxRow, boxCol) && color; //&& agentColor.toString()==boxColors[box].toString();
+                    //Boolean color = (agentColor.toString() == boxColors[agent].toString());
+                    return this.cellIsFree(boxRow, boxCol); //&& agentColor.toString()==boxColors[box].toString();
                 }
                 else return false;
 
